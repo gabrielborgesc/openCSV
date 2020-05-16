11da82f642b285049d4b9205eb00666f8d5ab8b6
Plenamente <gustavoplenamente@gmail.com>
2020-05-16 16:54:37 -0300

    First commit

---
+++ src/main/java/com/aps2/analisedadosensino/AnaliseDadosEnsinoApplication.java
@@ -0,0 +1,13 @@
+package com.aps2.analisedadosensino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseDadosEnsinoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnaliseDadosEnsinoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aps2/analisedadosensino/AnaliseDadosEnsinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aps2.analisedadosensino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnaliseDadosEnsinoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
